9cf73850f8e00a3c28f03c07597b47d170b23678
prmH <ablyashev.artur@gmail.com>
2017-09-17 20:50:59 +0500

    init

---
+++ project/cvSchool/cvSchool/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace cvSchool
+{
+    partial class cv_school
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btFragment = new System.Windows.Forms.Button();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.bOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.fdbMain = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnGrey = new System.Windows.Forms.Button();
+            this.btnFlip = new System.Windows.Forms.Button();
+            this.btnNormalize = new System.Windows.Forms.Button();
+            this.btnNoise = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnNoise);
+            this.panel1.Controls.Add(this.btnNormalize);
+            this.panel1.Controls.Add(this.btnFlip);
+            this.panel1.Controls.Add(this.btnGrey);
+            this.panel1.Controls.Add(this.btFragment);
+            this.panel1.Controls.Add(this.tbLog);
+            this.panel1.Controls.Add(this.bOpen);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tbPath);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(552, 261);
+            this.panel1.TabIndex = 0;
+            // 
+            // btFragment
+            // 
+            this.btFragment.Location = new System.Drawing.Point(12, 54);
+            this.btFragment.Name = "btFragment";
+            this.btFragment.Size = new System.Drawing.Size(82, 23);
+            this.btFragment.TabIndex = 4;
+            this.btFragment.Text = "1. Fragments";
+            this.btFragment.UseVisualStyleBackColor = true;
+            this.btFragment.Click += new System.EventHandler(this.btFragment_Click);
+            // 
+            // tbLog
+            // 
+            this.tbLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbLog.Location = new System.Drawing.Point(12, 83);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ReadOnly = true;
+            this.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbLog.Size = new System.Drawing.Size(532, 166);
+            this.tbLog.TabIndex = 3;
+            // 
+            // bOpen
+            // 
+            this.bOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bOpen.Location = new System.Drawing.Point(519, 28);
+            this.bOpen.Name = "bOpen";
+            this.bOpen.Size = new System.Drawing.Size(25, 21);
+            this.bOpen.TabIndex = 2;
+            this.bOpen.Text = "...";
+            this.bOpen.UseVisualStyleBackColor = true;
+            this.bOpen.Click += new System.EventHandler(this.bOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Укажите каталог размещения файлов:";
+            // 
+            // tbPath
+            // 
+            this.tbPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPath.Enabled = false;
+            this.tbPath.Location = new System.Drawing.Point(12, 28);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(501, 20);
+            this.tbPath.TabIndex = 0;
+            this.tbPath.Text = "~\\cv_school_test";
+            // 
+            // btnGrey
+            // 
+            this.btnGrey.Location = new System.Drawing.Point(100, 54);
+            this.btnGrey.Name = "btnGrey";
+            this.btnGrey.Size = new System.Drawing.Size(82, 23);
+            this.btnGrey.TabIndex = 5;
+            this.btnGrey.Text = "2. GreyScale";
+            this.btnGrey.UseVisualStyleBackColor = true;
+            this.btnGrey.Click += new System.EventHandler(this.btnGrey_Click);
+            // 
+            // btnFlip
+            // 
+            this.btnFlip.Location = new System.Drawing.Point(188, 54);
+            this.btnFlip.Name = "btnFlip";
+            this.btnFlip.Size = new System.Drawing.Size(82, 23);
+            this.btnFlip.TabIndex = 6;
+            this.btnFlip.Text = "3. Flip";
+            this.btnFlip.UseVisualStyleBackColor = true;
+            this.btnFlip.Click += new System.EventHandler(this.btnFlip_Click);
+            // 
+            // btnNormalize
+            // 
+            this.btnNormalize.Location = new System.Drawing.Point(276, 54);
+            this.btnNormalize.Name = "btnNormalize";
+            this.btnNormalize.Size = new System.Drawing.Size(82, 23);
+            this.btnNormalize.TabIndex = 7;
+            this.btnNormalize.Text = "4. Normalize";
+            this.btnNormalize.UseVisualStyleBackColor = true;
+            this.btnNormalize.Click += new System.EventHandler(this.btnNormalize_Click);
+            // 
+            // btnNoise
+            // 
+            this.btnNoise.Location = new System.Drawing.Point(364, 54);
+            this.btnNoise.Name = "btnNoise";
+            this.btnNoise.Size = new System.Drawing.Size(82, 23);
+            this.btnNoise.TabIndex = 8;
+            this.btnNoise.Text = "5. Noise";
+            this.btnNoise.UseVisualStyleBackColor = true;
+            this.btnNoise.Click += new System.EventHandler(this.btnNoise_Click);
+            // 
+            // cv_school
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 261);
+            this.Controls.Add(this.panel1);
+            this.Name = "cv_school";
+            this.Text = "cv_school";
+            this.Load += new System.EventHandler(this.cv_school_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button bOpen;
+        private System.Windows.Forms.FolderBrowserDialog fdbMain;
+        private System.Windows.Forms.TextBox tbLog;
+        private System.Windows.Forms.Button btFragment;
+        private System.Windows.Forms.Button btnGrey;
+        private System.Windows.Forms.Button btnFlip;
+        private System.Windows.Forms.Button btnNormalize;
+        private System.Windows.Forms.Button btnNoise;
+    }
+}
+
---
+++ project/cvSchool/cvSchool/Form1.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Media;
+using System.Drawing.Imaging;
+
+namespace cvSchool
+{
+    public partial class cv_school : Form
+    {
+        string _mainPath = AppContext.BaseDirectory;        
+
+        public cv_school()
+        {
+            InitializeComponent();
+        }
+
+        void writeLog(string info, bool isNew = false)
+        {
+            if (isNew) tbLog.AppendText("\r\n");
+            tbLog.AppendText(info + "\r\n");
+        }
+
+        private void bOpen_Click(object sender, EventArgs e)
+        {
+            if (fdbMain.ShowDialog() == DialogResult.OK)
+            {
+                _mainPath = fdbMain.SelectedPath;
+                tbPath.Text = _mainPath;
+                tbLog.Clear();
+                btFragment.Enabled = true;
+                writeLog(String.Format("Исходный каталог: {0}", _mainPath));
+            }
+        }
+
+        private void cv_school_Load(object sender, EventArgs e)
+        {
+            string _folder = "cv_school_test";            
+            _mainPath = _mainPath.Substring(0, _mainPath.IndexOf(_folder) + _folder.Length);
+            tbPath.Text = _mainPath;
+            writeLog(String.Format("Исходный каталог: {0}", _mainPath));
+        }        
+
+        private void btFragment_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                writeLog("1. Выделение фрагментов", true);
+                string _srcFolder = _mainPath + @"\images\";
+                string _frgFolder = _mainPath + @"\fragments\";
+                string _antFolder = _mainPath + @"\annotations\";
+
+                Int32 i = 0;
+                Int32 x = 0;
+
+                Directory.CreateDirectory(_frgFolder);
+                writeLog(String.Format("Каталог с исходными картинками: {0}", _srcFolder));
+                writeLog(String.Format("Каталог с фрагментами: {0}", _frgFolder));
+
+                string[] files = Directory.GetFiles(_srcFolder);
+                writeLog(String.Format("Количество файлов в каталоге: {0}", files.Length));
+
+                foreach (string f in files)
+                {
+                    FileInfo file = new FileInfo(f);
+                    Bitmap _src = new Bitmap(f);
+
+                    x = 0;
+                    i += 1;                    
+                    writeLog(String.Format("Обработка {0}-го файла: {1}", i, file.Name), true);
+
+                    var _antFile = file.Name.Substring(0, file.Name.IndexOf(file.Extension));
+                    _antFile = _antFolder + _antFile + ".txt";
+                    writeLog(String.Format("Файл с аннотациями: {0}", _antFile));
+
+                    string[] _lines = File.ReadAllLines(_antFile);
+                    writeLog(String.Format("Файл с аннотациями содержит {0} фрагмента", _lines.Length));
+
+                    foreach (string _line in _lines)
+                    {
+                        writeLog(String.Format("Обработка фрагмента с координатами {0}", _line));
+                        var _frgFile = file.Name.Substring(0, file.Name.IndexOf(file.Extension));
+                        _frgFile = _frgFolder + _frgFile + "_" + x + ".png";
+                        string[] _coord = _line.Split(',');
+                        Rectangle rectangle = new Rectangle(Int32.Parse(_coord[0]), 
+                                                            Int32.Parse(_coord[1]),
+                                                            Int32.Parse(_coord[2]),
+                                                            Int32.Parse(_coord[3]));
+                        Bitmap _dst = CropImage(_src, rectangle);
+                        _dst.Save(_frgFile);
+                        writeLog(String.Format("Фрагент сохранен: {0}", _frgFile));
+                        x += 1;
+                    }                   
+                }                
+                writeLog("Выделение фрагментов успешно завершено!");
+            }
+            catch
+            {
+                writeLog("Упс! Произошла ошибка");
+            }
+        }
+
+        public Bitmap CropImage(Bitmap source, Rectangle section)
+        {
+            Bitmap bmp = new Bitmap(section.Width, section.Height);
+            Graphics g = Graphics.FromImage(bmp);            
+            g.DrawImage(source, 0, 0, section, GraphicsUnit.Pixel);
+
+            return bmp;
+        }
+
+        private void btnGrey_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                writeLog("2. Преобразование greyscale", true);
+                string _srcFolder = _mainPath + @"\fragments\";
+                string _grsFolder = _mainPath + @"\fragments_greyscale\";
+
+                Directory.CreateDirectory(_grsFolder);
+                writeLog(String.Format("Каталог с исходными фрагментами: {0}", _srcFolder));
+                writeLog(String.Format("Каталог с преобразованными фрагментами: {0}", _grsFolder));
+
+                string[] files = Directory.GetFiles(_srcFolder);
+                writeLog(String.Format("Количество исходных фрагментов: {0}", files.Length));
+
+                foreach (string f in files)
+                {
+                    FileInfo file = new FileInfo(f);
+                    Bitmap _src = new Bitmap(f);
+                    Bitmap _dst;
+                    Int32 x, y;
+
+                    writeLog(String.Format("Обработка фрагмента: {0}", file.Name));
+
+                    for (x = 0; x < _src.Width; x++)
+                    {
+                        for (y = 0; y < _src.Height; y++)
+                        {
+                            Color pixelColor = _src.GetPixel(x, y);
+                            int grayScale = (int)((pixelColor.R * 0.3) + (pixelColor.G * 0.59) + (pixelColor.B * 0.11));
+                            Color newColor = Color.FromArgb(pixelColor.A, grayScale, grayScale, grayScale);
+                            _src.SetPixel(x, y, newColor);
+                        }
+                    }
+                    _dst = _src;
+
+                    var _grsFile = file.Name.Substring(0, file.Name.IndexOf(file.Extension));
+                    _grsFile = _grsFolder + _grsFile + "_grey" + ".png";
+                    _dst.Save(_grsFile);
+                    writeLog(String.Format("Преобразованный фрагент сохранен: {0}", _grsFile));                    
+                }
+                writeLog("Преобразование greyscale успешно завершено!");
+            }
+            catch
+            {
+                writeLog("Упс! Произошла ошибка");
+            }            
+        }
+
+        private void btnFlip_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                writeLog("3. Отображение фрагментов по горизонтали", true);
+                string _srcFolder = _mainPath + @"\fragments\";
+                string _frfFolder = _mainPath + @"\fragments_flip\";
+
+                Directory.CreateDirectory(_frfFolder);
+                writeLog(String.Format("Каталог с исходными фрагментами: {0}", _srcFolder));
+                writeLog(String.Format("Каталог с преобразованными фрагментами: {0}", _frfFolder));
+
+                string[] files = Directory.GetFiles(_srcFolder);
+                writeLog(String.Format("Количество исходных фрагментов: {0}", files.Length));
+
+                foreach (string f in files)
+                {
+                    FileInfo file = new FileInfo(f);
+                    Bitmap _src = new Bitmap(f);
+                    Bitmap _dst;
+                    
+                    writeLog(String.Format("Обработка фрагмента: {0}", file.Name));
+
+                    _src.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    _dst = _src;
+
+                    var _frfFile = file.Name.Substring(0, file.Name.IndexOf(file.Extension));
+                    _frfFile = _frfFolder + _frfFile + "_flip" + ".png";
+                    _dst.Save(_frfFile);
+                    writeLog(String.Format("Преобразованный фрагент сохранен: {0}", _frfFile));
+                }
+                writeLog("Преобразование фрагментов по горизонтали успешно завершено!");
+            }
+            catch
+            {
+                writeLog("Упс! Произошла ошибка");
+            }            
+        }
+
+        private void btnNormalize_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                writeLog("4. Нормализация изображений", true);
+                string _srcFolder = _mainPath + @"\fragments_flip\";
+                string _nrmFolder = _mainPath + @"\fragments_norm\";
+
+                Directory.CreateDirectory(_nrmFolder);
+                writeLog(String.Format("Каталог с исходными фрагментами: {0}", _srcFolder));
+                writeLog(String.Format("Каталог с преобразованными фрагментами: {0}", _nrmFolder));
+
+                string[] files = Directory.GetFiles(_srcFolder);
+                writeLog(String.Format("Количество исходных фрагментов: {0}", files.Length));
+
+                foreach (string f in files)
+                {
+                    FileInfo file = new FileInfo(f);
+                    Bitmap _src = new Bitmap(f);
+                    float minBrightness = 0;
+                    float maxBrightness = 255;
+
+                    writeLog(String.Format("Обработка фрагмента: {0}", file.Name));
+
+                    Bitmap _dst = new Bitmap(_src.Width, _src.Height);
+
+                    for (int x = 0; x < _src.Width; ++x)
+                    {
+                        for (int y = 0; y < _src.Height; ++y)
+                        {
+                            float pixelBrightness = _src.GetPixel(x, y).GetBrightness();
+                            if (x == 0 && y == 0)
+                            {
+                                minBrightness = pixelBrightness;
+                                maxBrightness = pixelBrightness;
+                            }
+
+                            minBrightness = Math.Min(minBrightness, pixelBrightness);
+                            maxBrightness = Math.Max(maxBrightness, pixelBrightness);
+                        }
+                    }
+
+                    for (int x = 0; x < _src.Width; x++)
+                    {
+                        for (int y = 0; y < _src.Height; y++)
+                        {
+                            Color pixelColor = _src.GetPixel(x, y);
+                            float normalizedPixelBrightness = (pixelColor.GetBrightness() - minBrightness)*((1 - 0)/ (maxBrightness - minBrightness));
+                            Color normalizedPixelColor = ColorFromAhsb(pixelColor.A, pixelColor.GetHue(), pixelColor.GetSaturation(), normalizedPixelBrightness);
+
+                            _dst.SetPixel(x, y, normalizedPixelColor);
+                        }
+                    }
+
+                    var _nrmFile = file.Name.Substring(0, file.Name.IndexOf(file.Extension));
+                    _nrmFile = _nrmFolder + _nrmFile + "_norm" + ".png";
+                    _dst.Save(_nrmFile);
+                    writeLog(String.Format("Преобразованный фрагент сохранен: {0}", _nrmFile));
+                }
+                writeLog("Нормализация фрагментов успешно завершена!");
+            }
+            catch
+            {
+                writeLog("Упс! Произошла ошибка");
+            }
+        }
+        public static Color ColorFromAhsb(int a, float h, float s, float b)
+        {
+            if (0 > a || 255 < a)
+            {
+                throw new ArgumentOutOfRangeException("a", a, "InvalidAlpha");
+            }
+            if (0f > h || 360f < h)
+            {
+                throw new ArgumentOutOfRangeException("h", h, "InvalidHue");
+            }
+            if (0f > s || 1f < s)
+            {
+                throw new ArgumentOutOfRangeException("s", s, "InvalidSaturation");
+            }
+            if (0f > b || 1f < b)
+            {
+                throw new ArgumentOutOfRangeException("b", b, "InvalidBrightness");
+            }
+
+            if (0 == s)
+            {
+                return Color.FromArgb(a, Convert.ToInt32(b * 255), Convert.ToInt32(b * 255), Convert.ToInt32(b * 255));
+            }
+
+            float fMax, fMid, fMin;
+            int iSextant, iMax, iMid, iMin;
+
+            if (0.5 < b)
+            {
+                fMax = b - (b * s) + s;
+                fMin = b + (b * s) - s;
+            }
+            else
+            {
+                fMax = b + (b * s);
+                fMin = b - (b * s);
+            }
+
+            iSextant = (int)Math.Floor(h / 60f);
+            if (300f <= h)
+            {
+                h -= 360f;
+            }
+            h /= 60f;
+            h -= 2f * (float)Math.Floor(((iSextant + 1f) % 6f) / 2f);
+            if (0 == iSextant % 2)
+            {
+                fMid = h * (fMax - fMin) + fMin;
+            }
+            else
+            {
+                fMid = fMin - h * (fMax - fMin);
+            }
+
+            iMax = Convert.ToInt32(fMax * 255);
+            iMid = Convert.ToInt32(fMid * 255);
+            iMin = Convert.ToInt32(fMin * 255);
+
+            switch (iSextant)
+            {
+                case 1:
+                    return Color.FromArgb(a, iMid, iMax, iMin);
+                case 2:
+                    return Color.FromArgb(a, iMin, iMax, iMid);
+                case 3:
+                    return Color.FromArgb(a, iMin, iMid, iMax);
+                case 4:
+                    return Color.FromArgb(a, iMid, iMin, iMax);
+                case 5:
+                    return Color.FromArgb(a, iMax, iMin, iMid);
+                default:
+                    return Color.FromArgb(a, iMax, iMid, iMin);
+            }
+        }
+
+        private void btnNoise_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                writeLog("5. Добавление шума", true);
+                string _srcFolder = _mainPath + @"\fragments_greyscale\";
+                string _frnFolder = _mainPath + @"\fragments_noise\";
+
+                Directory.CreateDirectory(_frnFolder);
+                writeLog(String.Format("Каталог с исходными фрагментами: {0}", _srcFolder));
+                writeLog(String.Format("Каталог с преобразованными фрагментами: {0}", _frnFolder));
+
+                string[] files = Directory.GetFiles(_srcFolder);
+                writeLog(String.Format("Количество исходных фрагментов: {0}", files.Length));
+
+                foreach (string f in files)
+                {
+                    FileInfo file = new FileInfo(f);
+                    Bitmap _src = new Bitmap(f);                    
+
+                    writeLog(String.Format("Обработка фрагмента: {0}", file.Name));
+
+                    Bitmap _dst = addNoise(_src, 20);                                
+
+                    var _frnFile = file.Name.Substring(0, file.Name.IndexOf(file.Extension));
+                    _frnFile = _frnFolder + _frnFile + "_noise" + ".png";
+                    _dst.Save(_frnFile);
+                    writeLog(String.Format("Преобразованный фрагент сохранен: {0}", _frnFile));
+                }
+                writeLog("Добавление шума успешно завершена!");
+            }
+            catch
+            {
+                writeLog("Упс! Произошла ошибка");
+            }
+        }
+
+        public static Bitmap addNoise(Bitmap src, int amount)
+        {
+            Bitmap dst = new Bitmap(src.Width, src.Height);           
+            Random r = new Random();
+
+            for (int x = 0; x < src.Width; ++x)
+            {
+                for (int y = 0; y < src.Height; ++y)
+                {
+                    Color pixelColor = src.GetPixel(x, y);
+                    int R = pixelColor.R + r.Next(-amount, amount + 1);
+                    int G = pixelColor.G + r.Next(-amount, amount + 1);
+                    int B = pixelColor.B + r.Next(-amount, amount + 1);
+                    R = R > 255 ? 255 : R;
+                    R = R < 0 ? 0 : R;
+                    G = G > 255 ? 255 : G;
+                    G = G < 0 ? 0 : G;
+                    B = B > 255 ? 255 : B;
+                    B = B < 0 ? 0 : B;
+                    Color newPixelColor = Color.FromArgb(R, G, B);
+                    dst.SetPixel(x, y, newPixelColor);
+                }
+            }            
+            return dst;
+        }
+    }
+    
+}
